ad18a26b834776379a49f7de8da0beb00ed83f7d
96Emil <91869024+96Emil@users.noreply.github.com>
2022-11-24 10:56:46 +0100

    commit

--- src/components/ProductInfo.js
+++ src/components/ProductInfo.js
@@ -29,67 +29,68 @@ const ProductInfo = () => {
                 className="read-only-input"
                 value={`${post.product}`}
               />
-            </div>
-            <div>
-              <h2>Kvantitet</h2>
-              <input
-                readOnly
-                type="text"
-                name="quantity"
-                className="quantity-read-only-input"
-                value={`${post.quantity}`}
-              />
-              <div className="input-explainer">kg</div>
-            </div>
-            <div>
-              <h2>Pris</h2>
-              <input
-                readOnly
-                type="text"
-                name="price"
-                className="price-read-only-input"
-                value={`${post.price}`}
-              />
-              <div className="input-explainer">kr</div>
-            </div>
-            <div>
-              <h2>Beskrivning</h2>
-              <textarea
-                readOnly
-                type="text"
-                name="description"
-                className="read-only-input"
-                value={`${post.description}`}
-              />
-            </div>
-            <div>
-              <h2>Bild</h2>
-              <img
-                className="product-image"
-                key={post.image}
-                src={post.image}
-                alt="bild"
-              />
-            </div>
-            <div>
-              <h2>Plats för upphämtning</h2>
-              <input
-                readOnly
-                type="text"
-                name="place"
-                className="read-only-input"
-                value={`${post.place}`}
-              />
-            </div>
-            <div>
-              <h2>Telefon</h2>
-              <input
-                readOnly
-                type="text"
-                name="phone"
-                className="read-only-input"
-                value={`${post.phone}`}
-              />
+
+              <div>
+                <h2>Kvantitet</h2>
+                <input
+                  readOnly
+                  type="text"
+                  name="quantity"
+                  className="quantity-read-only-input"
+                  value={`${post.quantity}`}
+                />
+                <div className="input-explainer">kg</div>
+              </div>
+              <div>
+                <h2>Pris</h2>
+                <input
+                  readOnly
+                  type="text"
+                  name="price"
+                  className="price-read-only-input"
+                  value={`${post.price}`}
+                />
+                <div className="input-explainer">kr</div>
+              </div>
+              <div>
+                <h2>Beskrivning</h2>
+                <textarea
+                  readOnly
+                  type="text"
+                  name="description"
+                  className="read-only-input"
+                  value={`${post.description}`}
+                />
+              </div>
+              <div>
+                <h2>Bild</h2>
+                <img
+                  className="product-image"
+                  key={post.image}
+                  src={post.image}
+                  alt="bild"
+                />
+              </div>
+              <div>
+                <h2>Plats för upphämtning</h2>
+                <input
+                  readOnly
+                  type="text"
+                  name="place"
+                  className="read-only-input"
+                  value={`${post.place}`}
+                />
+              </div>
+              <div>
+                <h2>Telefon</h2>
+                <input
+                  readOnly
+                  type="text"
+                  name="phone"
+                  className="read-only-input"
+                  value={`${post.phone}`}
+                />
+              </div>
             </div>
             <div className="buttons">
               <Link to={`/ConfirmPurchase/${post.id}`} className="buy-button">
